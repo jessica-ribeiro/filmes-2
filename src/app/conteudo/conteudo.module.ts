25958c5149e0e715f9da0095926e84d0da976ad7
jessydipaula <jessicadepaula.stm@gmail.com>
2018-04-23 17:09:33 -0300

    Update

--- src/app/conteudo/conteudo.module.ts
+++ src/app/conteudo/conteudo.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 import { FilmesCardComponent } from './filmes-card/filmes-card.component';
+
 import { FilmeService } from './filme.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormPesquisaComponent } from './lista-filmes/form-pesquisa/form-pesquisa.component';
