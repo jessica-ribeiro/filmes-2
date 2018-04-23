7d09943f056465979475ec7740689e9413cab746
jessydipaula <jessicadepaula.stm@gmail.com>
2018-04-23 17:04:24 -0300

    Formulário & Output

--- src/app/conteudo/conteudo.module.ts
+++ src/app/conteudo/conteudo.module.ts
@@ -4,15 +4,19 @@ import { ListaFilmesComponent } from './lista-filmes/lista-filmes.component';
 import { FilmesCardComponent } from './filmes-card/filmes-card.component';
 import { FilmeService } from './filme.service';
 import { HttpClientModule } from '@angular/common/http';
+import { FormPesquisaComponent } from './lista-filmes/form-pesquisa/form-pesquisa.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   declarations: [
     ListaFilmesComponent, 
-    FilmesCardComponent],
+    FilmesCardComponent, FormPesquisaComponent],
   exports:[
     ListaFilmesComponent
   ],
